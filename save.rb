ec9a36fbd28bcf075bf515b5afa108c055cb95ce
Jorge <jorgejapao00@gmail.com>
2024-07-08 16:46:55 -0300

    Function to save game updated. Save level score updated

--- save.rb
+++ save.rb
@@ -3,11 +3,17 @@ require 'inifile'
 $save = IniFile.load('save.ini')
 
 
-def save_game(level, score)
+def save_game(current_level)
+  $save['current']['status'] = current_level.to_s
+  $save.save
+end
+
+def save_level(level, score)
   $save[level.to_s]['status'] = score.to_s
   $save.save
 end
 
 if __FILE__ == $0
-  save_game(1, 0.423)
+  save_game(3)
+  save_level(1, 0.423)
 end
