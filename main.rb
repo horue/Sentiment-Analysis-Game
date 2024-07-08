f0f94e264bd0371ef3e5a5b0c319ca56a4e184dd
Jorge <jorgejapao00@gmail.com>
2024-07-08 14:10:51 -0300

    Main game structure added

---
+++ main.rb
@@ -0,0 +1,21 @@
+require 'Sentimental'
+
+analyzer = Sentimental.new
+analyzer.load_defaults
+
+
+score = 0
+
+
+loop do
+  print "Enter a message on how you are feeling: "
+  message = gets
+  sentiment = analyzer.sentiment message
+
+  if sentiment == :positive then
+    score += 1
+    puts "Positive Sentiment! Score: #{score}"
+  elsif sentiment == :negative then
+    puts "Negative Sentiment... Score: #{score}"
+  end
+end
