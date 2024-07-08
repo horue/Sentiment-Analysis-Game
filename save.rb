896fad2eca663ae44a3dee55a6659459d74dbae3
Jorge <jorgejapao00@gmail.com>
2024-07-08 16:43:14 -0300

    Save variable is now global. If file added for testing

--- save.rb
+++ save.rb
@@ -1,12 +1,13 @@
 require 'inifile'
 
-save = IniFile.load('save.ini')
+$save = IniFile.load('save.ini')
 
 
 def save_game(level, score)
-  save = IniFile.load('save.ini')
-  save[level.to_s]['status'] = score.to_s
-  save.save
+  $save[level.to_s]['status'] = score.to_s
+  $save.save
 end
 
-save_game(1, 0.421)
+if __FILE__ == $0
+  save_game(1, 0.423)
+end
