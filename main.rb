dc86f1475a307bb6ff498c7dfa37b5064a0452ad
Jorge <jorgejapao00@gmail.com>
2024-07-08 15:12:36 -0300

    Game moved to a function

--- main.rb
+++ main.rb
@@ -7,26 +7,29 @@ analyzer.load_defaults
 
 score = 0.1
 
+def game
+  loop do
+    print "Enter a message on how you are feeling: "
+    message = gets
+    sentiment = analyzer.sentiment message
+    sentiment_score = analyzer.score message
 
-loop do
-  print "Enter a message on how you are feeling: "
-  message = gets
-  sentiment = analyzer.sentiment message
-  sentiment_score = analyzer.score message
+    if sentiment == :positive then
+      score += sentiment_score
+      save_game(1, score)
+      puts "Positive Sentiment! Score: #{score}"
+    elsif sentiment == :negative then
+      score += sentiment_score
+      save_game(1, score)
+      puts "Negative Sentiment... Score: #{score}"
+    end
 
-  if sentiment == :positive then
-    score += sentiment_score
-    save_game(1, score)
-    puts "Positive Sentiment! Score: #{score}"
-  elsif sentiment == :negative then
-    score += sentiment_score
-    save_game(1, score)
-    puts "Negative Sentiment... Score: #{score}"
-  end
-
-  if score <-1 then
-    puts "It looks like you are not ready for this."
-    puts "See you later."
-  break
+    if score <-1 then
+      puts "It looks like you are not ready for this."
+      puts "See you later."
+    break
+    end
   end
 end
+
+game
