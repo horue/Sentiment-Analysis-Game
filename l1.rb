672224e4617c6275ebf098dac697f30453dac9cc
Jorge <jorgejapao00@gmail.com>
2024-07-08 16:58:56 -0300

    Level 1 test created

---
+++ l1.rb
@@ -0,0 +1,33 @@
+require 'Sentimental'
+require_relative 'save'
+
+analyzer = Sentimental.new
+analyzer.load_defaults
+
+
+score = 0.1
+
+
+questions = [
+  'Do you think humanity would win a war against machines? ',
+  'a',
+  'b'
+]
+
+
+for a in questions do
+  print a
+  message = gets
+  sentiment = analyzer.sentiment message
+  sentiment_score = analyzer.score message
+
+  if sentiment == :positive then
+    score += sentiment_score
+    save_level(1, score)
+    puts "Positive Sentiment! Score: #{score}"
+  elsif sentiment == :negative then
+    score += sentiment_score
+    save_level(1, score)
+    puts "Negative Sentiment... Score: #{score}"
+  end
+end
