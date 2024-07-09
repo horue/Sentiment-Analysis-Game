effe951d0f58633152aec87a986a77fdad5a28b8
Jorge <jorgejapao00@gmail.com>
2024-07-09 14:27:23 -0300

    Game main screen created

---
+++ main_screen.rb
@@ -0,0 +1,30 @@
+require 'tty-prompt'
+require_relative 'save'
+
+
+
+def main
+    prompt = TTY::Prompt.new
+    choices = [
+    {name: 'New Game', value: 1},
+    {name: 'Continue', value: 2},
+    {name: 'Help', value: 3},
+    {name: 'Exit', value: 4}
+    ]
+
+    selected = prompt.select('Experimentalist Serena - Ruby Version!', choices)
+
+    case selected
+    when 1
+    puts 'Starting a New Game...'
+    when 2
+    puts 'Continuing the game...'
+    puts load_game
+    when 3
+    puts 'Displaying help...'
+    when 4
+    puts 'Exiting the game...'
+    end
+end
+
+main
